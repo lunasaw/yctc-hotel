883a2dd08368a915707e1b5dd3eaf8cdc423cea3
czy1024 <iszychen@gmail.com>
2020-01-18 23:45:15 +0800

    消息通知系统1.0

--- src/main/java/com/altersoftware/hotel/controller/rest/UserRestController.java
+++ src/main/java/com/altersoftware/hotel/controller/rest/UserRestController.java
@@ -3,7 +3,6 @@ package com.altersoftware.hotel.controller.rest;
 
 import com.altersoftware.hotel.entity.ResultDO;
 import com.altersoftware.hotel.entity.UserDO;
-import com.altersoftware.hotel.vo.UserInformationVO;
 
 public interface UserRestController {
 
@@ -23,12 +22,6 @@ public interface UserRestController {
      */
     ResultDO<Long> signin(UserDO userDO);
 
-    /**
-     * getUserInformationById的rest方法
-     *
-     * @param id
-     * @return
-     */
-    ResultDO<UserInformationVO> getUserInformationById(long id);
+
 
 }
