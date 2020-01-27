a1f481dfb87255ead2bebb0c20cb48cd2ce2d3a0
improve <15696756582@163.com>
2020-01-27 16:57:01 +0800

    dev -improve

--- src/main/java/com/altersoftware/hotel/constant/ConstantHolder.java
+++ src/main/java/com/altersoftware/hotel/constant/ConstantHolder.java
@@ -3,11 +3,11 @@ package com.altersoftware.hotel.constant;
 public class ConstantHolder {
 
     /** 网站的产品名 */
-    public static final String PRODUCT_NAME = "智慧教室";
+    public static final String PRODUCT_NAME      = "智慧酒店";
 
     /** 短信提示短信标题 */
-    public static final String STATE_SMS_TITLE = "【哈沐信息技术】";
+    public static final String STATE_SMS_TITLE   = "";
 
     /** 短信提示正文 */
-    public static final String STATE_SMS_CONTENT = "同学，请你集中注意力听讲！";
+    public static final String STATE_SMS_CONTENT = "";
 }
