26d48e953fbd5a870df6e473866bdae37065d44d
czy1024 <iszychen@gmail.com>
2020-01-30 22:08:09 +0800

    dev HotelService /-improve

---
+++ src/main/java/com/altersoftware/hotel/service/HotelService.java
@@ -0,0 +1,32 @@
+package com.altersoftware.hotel.service;
+
+import com.altersoftware.hotel.entity.HotelDO;
+import com.altersoftware.hotel.entity.ResultDO;
+
+/**
+ * @author czy@win10
+ * @date 2020/1/30 21:42
+ */
+public interface HotelService {
+
+    /**
+     * 查看酒店信息
+     * 
+     * @return
+     */
+    ResultDO<HotelDO> showHotel(long Id);
+
+    /**
+     * 修改酒店信息
+     * 
+     * @return
+     */
+    ResultDO<Void> updateHotel(HotelDO hotelDO);
+
+    /**
+     * 增加一条酒店信息
+     * 
+     * @return
+     */
+    ResultDO<Void> insertHotel(HotelDO hotelDO);
+}
---
+++ src/main/java/com/altersoftware/hotel/vo/HotelVO.java
@@ -0,0 +1,9 @@
+package com.altersoftware.hotel.vo;
+
+/**
+ * @author czy@win10
+ * @date 2020/1/30 22:06
+ */
+public class HotelVO {
+
+}
