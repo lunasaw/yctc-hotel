328fab83dc31368d429fa16ce6c575aca13ea8a1
improve <15696756582@163.com>
2020-01-30 16:16:20 +0800

    code review /-improve

--- src/main/java/com/altersoftware/hotel/dao/MealdistributionDAO.java
+++ src/main/java/com/altersoftware/hotel/dao/MealdistributionDAO.java
@@ -1,7 +1,12 @@
 package com.altersoftware.hotel.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author czy@win10
  * @date 2020/1/30 16:03
  */
-public interface MealdistributionDAO {}
+@Mapper
+public interface MealdistributionDAO {
+
+}
