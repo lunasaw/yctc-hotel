21d6ccc768fdc5834274be0fd319e6bbf92e8b71
czy1024 <iszychen@gmail.com>
2020-02-02 10:48:00 +0800

    menu控制层

--- src/main/java/com/altersoftware/hotel/controller/rest/impl/MenuRestControllerImpl.java
+++ src/main/java/com/altersoftware/hotel/controller/rest/impl/MenuRestControllerImpl.java
@@ -2,8 +2,13 @@ package com.altersoftware.hotel.controller.rest.impl;
 
 import java.util.List;
 
+import com.altersoftware.hotel.constant.ResultCode;
+import com.altersoftware.hotel.entity.GoodsDO;
+import com.altersoftware.hotel.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,41 +17,146 @@ import com.altersoftware.hotel.entity.MenuDO;
 import com.altersoftware.hotel.entity.ResultDO;
 
 /**
- * @author czy@win10
- * @date 2020/2/1 21:42
+ * @author hzx
+ * @date
  */
 @RestController
 @ComponentScan({"com.altersoftware.hotel.service"})
 @RequestMapping("/menuinfo")
 public class MenuRestControllerImpl implements MenuRestController {
 
+    @Autowired
+    MenuService menuService;
+    /**
+     * 添加一条菜品
+     *
+     * @param menuDO
+     * @return
+     */
     @Override
     @PostMapping("insert-menu")
-    public ResultDO<Void> insert(MenuDO menuDO) {
-        return null;
+    public ResultDO<Void> insert(@RequestBody MenuDO menuDO) {
+        //参数校验
+        if (menuDO.getId() <= 0 || menuDO.getNumbers() <= 0 || menuDO.getPrice() <= 0){
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<Void> voidResultDO = menuService.insert(menuDO);
+        if (voidResultDO.isSuccess() == false) {
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS);
+        }
     }
 
+    /**
+     * id返回菜品信息
+     *
+     * @param id
+     * @return
+     */
     @Override
     @PostMapping("show-menu")
     public ResultDO<MenuDO> showById(long id) {
-        return null;
+        //参数校验
+        if (id <= 0){
+            return new ResultDO<MenuDO>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<MenuDO> menuDOResultDO = menuService.showById(id);
+        if (menuDOResultDO.isSuccess() == false){
+            return new ResultDO<MenuDO>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        }
+        else {
+            MenuDO doResultDOModule = menuDOResultDO.getModule();
+            return new ResultDO<MenuDO>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS, doResultDOModule);
+        }
     }
 
+    /**
+     * 更新菜品信息
+     *
+     * @param menuDO
+     * @return
+     */
     @Override
     @PostMapping("updae-menu")
-    public ResultDO<Void> update(MenuDO menuDO) {
-        return null;
+    public ResultDO<Void> update(@RequestBody MenuDO menuDO) {
+        //参数校验
+        if (menuDO.getId() <= 0 || menuDO.getNumbers() <= 0 || menuDO.getPrice() <= 0){
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<MenuDO> menuDOResultDO = menuService.showById(menuDO.getId());
+        if (menuDOResultDO.isSuccess() == false){
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        }
+        else {
+            MenuDO doResultDOModule = menuDOResultDO.getModule();
+            doResultDOModule.setId(menuDO.getId());
+            doResultDOModule.setName(menuDO.getName());
+            doResultDOModule.setNumbers(menuDO.getNumbers());
+            doResultDOModule.setPicture(menuDO.getPicture());
+            doResultDOModule.setPrice(menuDO.getPrice());
+
+            ResultDO<Void> voidResultDO = menuService.update(doResultDOModule);
+            if (voidResultDO.isSuccess() == false){
+                return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                        ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+            }
+            else {
+                return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS);
+            }
+        }
     }
 
+    /**
+     * 删除菜品信息
+     *
+     * @param id
+     * @return
+     */
     @Override
     @PostMapping("delete-menu")
     public ResultDO<Void> delete(long id) {
-        return null;
+        //参数校验
+        if (id <= 0){
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<Void> voidResultDO = menuService.delete(id);
+        if (voidResultDO.isSuccess() == false){
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        }
+        else {
+            return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS);
+        }
     }
 
+    /**
+     * 返回所有菜品信息
+     *
+     * @return
+     */
     @Override
     @PostMapping("show-listmenu")
     public ResultDO<List<MenuDO>> showAll() {
-        return null;
+        ResultDO<List<MenuDO>> listResultDO = menuService.showAll();
+        if (listResultDO.isSuccess() == false){
+            return new ResultDO<List<MenuDO>>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        }
+        else {
+            List<MenuDO> menuDOList = listResultDO.getModule();
+            return new ResultDO<List<MenuDO>>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS, menuDOList);
+        }
     }
 }
