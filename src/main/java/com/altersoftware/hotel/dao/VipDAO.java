2865c8f4dffd244b1d5841eca33029b617ca7b92
improve <15696756582@163.com>
2020-02-04 14:54:31 +0800

    dev Vip/GradeDAO /-improve

---
+++ src/main/java/com/altersoftware/hotel/dao/VipDAO.java
@@ -0,0 +1,66 @@
+package com.altersoftware.hotel.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.*;
+
+import com.altersoftware.hotel.entity.VipDO;
+
+/**
+ * @author czy@win10
+ * @date 2020/2/4 14:12
+ */
+@Mapper
+public interface VipDAO {
+    /**
+     * 插入一条VIP会员信息
+     *
+     *
+     */
+    @Insert(" INSERT INTO tb_vip (id, customer_id, grade, pay_amount, create_time, modify_time ) "
+        +
+        "VALUES(#{id}, #{customerNumber}, #{grade}, #{amount}, now(), now()) ")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void insert(VipDO vipDO);
+
+    /**
+     * id查找VIP会员消息
+     *
+     * @param id
+     */
+    @Select("select id, customer_id, grade, pay_amount, create_time, modify_time from tb_vip where id=#{id}  ")
+    @Results({
+        @Result(property = "id", column = "id"),
+        @Result(property = "customerNumber", column = "customer_id"),
+        @Result(property = "grade", column = "grade"),
+        @Result(property = "amount", column = "pay_amount"),
+        @Result(property = "createTime", column = "create_time"),
+        @Result(property = "modifyTime", column = "modify_time")
+    })
+    VipDO getVipDOById(long id);
+
+    /**
+     * 更新VIP会员消息
+     *
+     * @param vipDO
+     */
+    @Update("update tb_vip  set customer_id=#{customerNumber}, grade=#{grade}, pay_amount=#{amount}, modify_time=now() where id=#{id}")
+    int update(VipDO vipDO);
+
+    /**
+     * 根据id删除一条VIP会员消息
+     *
+     * @param id
+     */
+    @Delete("DELETE FROM tb_vip WHERE id=#{id}")
+    int deleteById(long id);
+
+    /**
+     * 返回VIP会员number list
+     *
+     * @return
+     */
+    @Select("select customerNumber  from tb_vip ")
+    List<Long> getVipIdList();
+
+}
---
+++ src/main/java/com/altersoftware/hotel/dao/VipGradeDAO.java
@@ -0,0 +1,65 @@
+package com.altersoftware.hotel.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.*;
+
+import com.altersoftware.hotel.entity.VipGradeDO;
+
+/**
+ * @author czy@win10
+ * @date 2020/2/4 14:22
+ */
+@Mapper
+public interface VipGradeDAO {
+    /**
+     * 插入一条VIP会员权益信息
+     *
+     *
+     */
+    @Insert(" INSERT INTO tb_vip_grade (id, equity, grade, discount, create_time, modify_time ) "
+        +
+        "VALUES(#{id}, #{equity}, #{grade}, #{discount}, now(), now()) ")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void insert(VipGradeDO vipGradeDO);
+
+    /**
+     * id查找VIP会员权益消息
+     *
+     * @param id
+     */
+    @Select("select id, equity, grade, discount, create_time, modify_time from tb_vip_grade where id=#{id}  ")
+    @Results({
+        @Result(property = "id", column = "id"),
+        @Result(property = "equity", column = "equity"),
+        @Result(property = "grade", column = "grade"),
+        @Result(property = "discount", column = "discount"),
+        @Result(property = "createTime", column = "create_time"),
+        @Result(property = "modifyTime", column = "modify_time")
+    })
+    VipGradeDO getVipGradeDOById(long id);
+
+    /**
+     * 更新VIP会员权益消息
+     *
+     * @param vipGradeDO
+     */
+    @Update("update tb_vip_grade  set equity=#{equity}, grade=#{grade}, discount=#{discount}, modify_time=now() where id=#{id}")
+    int update(VipGradeDO vipGradeDO);
+
+    /**
+     * 根据id删除一条VIP会员权益消息
+     *
+     * @param id
+     */
+    @Delete("DELETE FROM tb_vip_grade WHERE id=#{id}")
+    int deleteById(long id);
+
+    /**
+     * 通过会员等级返回VIP会员权益list
+     *
+     * @return
+     */
+    @Select("select id, equity, grade, discount, create_time, modify_time from tb_vip_grade where grade=#{grade}")
+    List<VipGradeDO> getVipIdList(String grade);
+}
