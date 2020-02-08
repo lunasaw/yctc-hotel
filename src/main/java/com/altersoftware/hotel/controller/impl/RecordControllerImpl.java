d67ce89368c4e541bc5ea89b50252bf36241b5f0
czy1024 <iszychen@gmail.com>
2020-02-08 18:25:41 +0800

    dev AliPay /-improve

--- src/main/java/com/altersoftware/hotel/controller/impl/RecordControllerImpl.java
+++ src/main/java/com/altersoftware/hotel/controller/impl/RecordControllerImpl.java
@@ -2,6 +2,7 @@ package com.altersoftware.hotel.controller.impl;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.altersoftware.hotel.constant.TemplatePath;
@@ -19,5 +20,10 @@ public class RecordControllerImpl implements RecordController {
     @Override
     public String showRecord() {
         return TemplatePath.RECORD_INFO;
+    }
+
+    @GetMapping("page")
+    public String payPage() {
+        return "pay/index";
     }
 }
