00b425d79fd18bd4d141d9dd45ea23f6ddd513a1
HZX <1656370528@qq.com>
2020-02-11 15:49:19 +0800

    menuControllor

--- src/main/java/com/altersoftware/hotel/controller/rest/impl/RoomTypeRestControllerImpl.java
+++ src/main/java/com/altersoftware/hotel/controller/rest/impl/RoomTypeRestControllerImpl.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.altersoftware.hotel.constant.ResultCode;
 import com.altersoftware.hotel.controller.rest.RoomTypeRestController;
--- src/main/java/com/altersoftware/hotel/vo/OrderVO.java
+++ src/main/java/com/altersoftware/hotel/vo/OrderVO.java
@@ -1,9 +1,64 @@
 package com.altersoftware.hotel.vo;
 
+import java.util.Date;
+
 /**
- * @author Iszychen@win10
+ * @author hzx
  * @date 2020/2/10 22:55
  */
 public class OrderVO {
+
+    /** 菜品编号 */
+    private long   menuId;
+    /** 菜品份数 */
+    private int    numbers;
+    /** 客户编号 */
+    private long   customerId;
+    /** 创建时间 */
+    Date          createTime;
+    /** 修改时间 */
+    Date           modifyTime;
+
+
+    public long getMenuId() {
+        return menuId;
+    }
+
+    public void setMenuId(long menuId) {
+        this.menuId = menuId;
+    }
+
+    public int getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
 
 }
