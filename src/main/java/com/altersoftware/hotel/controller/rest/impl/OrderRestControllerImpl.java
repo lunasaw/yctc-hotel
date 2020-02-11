56b55e744e496974fbf38e1e2223106ea3f6fc88
czy1024 <iszychen@gmail.com>
2020-02-11 15:49:19 +0800

    menuControllor

--- src/main/java/com/altersoftware/hotel/controller/rest/impl/OrderRestControllerImpl.java
+++ src/main/java/com/altersoftware/hotel/controller/rest/impl/OrderRestControllerImpl.java
@@ -2,10 +2,15 @@ package com.altersoftware.hotel.controller.rest.impl;
 
 import java.util.List;
 
+import com.altersoftware.hotel.constant.ResultCode;
+import com.altersoftware.hotel.entity.RecordDO;
+import com.altersoftware.hotel.service.MenuService;
+import com.altersoftware.hotel.service.OrderService;
+import com.altersoftware.hotel.util.MenuOrder;
+import com.altersoftware.hotel.util.RandomNumber;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.altersoftware.hotel.controller.rest.OrderRestController;
 import com.altersoftware.hotel.entity.OrderDO;
@@ -13,53 +18,188 @@ import com.altersoftware.hotel.entity.ResultDO;
 import com.altersoftware.hotel.vo.OrderVO;
 
 /**
- * @author Iszychen@win10
+ * @author hzx
  * @date 2020/2/10 22:55
  */
 @RestController
 @ComponentScan({"com.altersoftware.hotel.service"})
 @RequestMapping("/order")
 public class OrderRestControllerImpl implements OrderRestController {
+    @Autowired
+    MenuService menuService;
 
+    @Autowired
+    OrderService orderService;
+    /**
+     * 根据构造的菜品订单存入数据库
+     *
+     * @param orderVO
+     * @return
+     */
     @Override
     @PostMapping("add-order")
-    public ResultDO<Void> createOrder(OrderVO orderVO) {
-        return null;
+    public ResultDO<Void> createOrder(@RequestBody OrderVO orderVO) {
+        //生成随机数
+        MenuOrder menuOrder = new MenuOrder();
+        //参数校验
+        if (orderVO.getNumbers() <= 0 || orderVO.getCustomerId() <= 0){
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+        //还要补充
+        ResultDO<Double> actualMoney = orderService.getActualMoney(orderVO.getCustomerId());
+        OrderDO orderDO = new OrderDO();
+        orderDO.setNumbers(orderVO.getNumbers());
+        orderDO.setCustomerId(orderVO.getCustomerId());
+        orderDO.setId(Long.parseLong(menuOrder.GetRandom()));
+        orderDO.setPayMoney(actualMoney.getModule());
+
+        ResultDO<Void> orderServiceOrder = orderService.createOrder(orderDO);
+
+        if (orderServiceOrder.isSuccess() == false) {
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS);
+        }
     }
 
+    /**
+     * 展示菜品订单信息
+     *
+     * @param id
+     */
     @Override
     @PostMapping("get-byid")
     public ResultDO<OrderDO> showOrder(long id) {
-        return null;
+        // 参数校验
+        if (id <= 0) {
+            return new ResultDO<OrderDO>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<OrderDO> orderDOResultDO = orderService.showOrder(id);
+        if (orderDOResultDO.isSuccess() == false) {
+            return new ResultDO<OrderDO>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<OrderDO>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,orderDOResultDO.getModule());
+        }
     }
 
+    /**
+     * 根据客户编号查询菜品订单信息List
+     *
+     * @param customerId
+     */
     @Override
     @PostMapping("get-listbycustomerid")
-    public ResultDO<List<OrderDO>> showOrderByCustomerId(long customerId) {
-        return null;
+    public ResultDO<List<OrderDO>> showOrderByCustomerId(@RequestParam(name = "customerId") long customerId) {
+        // 参数校验
+        if (customerId <= 0) {
+            return new ResultDO<List<OrderDO>>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<List<OrderDO>> listResultDO = orderService.showOrderByCustomerId(customerId);
+        if (listResultDO.isSuccess() == false) {
+            return new ResultDO<List<OrderDO>>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<List<OrderDO>>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,listResultDO.getModule());
+        }
     }
 
+    /**
+     * 修改菜品订单信息
+     *
+     * @param orderDO
+     */
     @Override
     @PostMapping("update")
-    public ResultDO<Void> updateOrder(OrderDO orderDO) {
-        return null;
+    public ResultDO<Void> updateOrder(@RequestBody OrderDO orderDO) {
+        //参数校验
+        if (orderDO.getNumbers() <= 0 || orderDO.getCustomerId() <= 0 || orderDO.getPayMoney() <= 0){
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+        ResultDO<OrderDO> orderDOResultDO = orderService.showOrder(orderDO.getId());
+        if (orderDOResultDO.isSuccess() == false) {
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            OrderDO doResultDOModule = orderDOResultDO.getModule();
+            doResultDOModule.setId(orderDO.getId());
+            doResultDOModule.setCustomerId(orderDO.getCustomerId());
+            doResultDOModule.setNumbers(orderDO.getNumbers());
+            doResultDOModule.setMenuId(orderDO.getMenuId());
+            doResultDOModule.setPayMoney(orderDO.getPayMoney());
+            doResultDOModule.setState(orderDO.getState());
+
+            ResultDO<Void> voidResultDO = orderService.updateOrder(doResultDOModule);
+            if (voidResultDO.isSuccess() == false) {
+                return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                        ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+            } else {
+                return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,voidResultDO.getModule());
+            }
+        }
     }
 
+    /**
+     * 返回所有订单编号
+     *
+     *
+     */
     @Override
     @PostMapping("get-idlist")
     public ResultDO<List<Long>> showOrderIdList() {
-        return null;
+        ResultDO<List<Long>> listResultDO = orderService.showOrderIdList();
+        if (listResultDO.isSuccess() == false) {
+            return new ResultDO<List<Long>>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<List<Long>>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,listResultDO.getModule());
+        }
     }
 
+    /**
+     * 删除菜品订单信息
+     *
+     * @return
+     */
     @Override
     @PostMapping("delete-byid")
     public ResultDO<Void> deleteById(long id) {
-        return null;
+        // 参数校验
+        if (id <= 0) {
+            return new ResultDO<Void>(false, ResultCode.PARAMETER_INVALID,
+                    ResultCode.MSG_PARAMETER_INVALID, null);
+        }
+
+        ResultDO<Void> voidResultDO = orderService.deleteById(id);
+        if (voidResultDO.isSuccess() == false) {
+            return new ResultDO<Void>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<Void>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS);
+        }
     }
 
+    /**
+     * 查询所有菜品订单信息
+     *
+     * @return
+     */
     @Override
     @PostMapping("get-list")
     public ResultDO<List<OrderDO>> getAll() {
-        return null;
+        ResultDO<List<OrderDO>> orderServiceAll = orderService.getAll();
+        if (orderServiceAll.isSuccess() == false) {
+            return new ResultDO<List<OrderDO>>(false, ResultCode.DATABASE_CAN_NOT_FIND_DATA,
+                    ResultCode.MSG_DATABASE_CAN_NOT_FIND_DATA, null);
+        } else {
+            return new ResultDO<List<OrderDO>>(true, ResultCode.SUCCESS, ResultCode.MSG_SUCCESS,orderServiceAll.getModule());
+        }
     }
 }
