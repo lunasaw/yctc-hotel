31f65d23116072b5bf1133741211a2fa8fd14ecb
czy1024 <iszychen@gmail.com>
2020-02-24 13:56:41 +0800

    页面

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -51,16 +51,13 @@ function dialog(message, callback) {
         },
     }).then((value) => {
         switch (value) {
-        case
-            "true";
-        :
+            case "true":
                 callback();
                 break;
             default:
                 break;
         }
-    };
-)
+    });
 }
 
 // 无参数post请求
