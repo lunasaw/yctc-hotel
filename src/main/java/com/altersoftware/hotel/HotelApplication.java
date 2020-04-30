a4bb5eb512e40d75a2fb5dc0a67c92383883bdba
luna <iszychen@gmail.com>
2020-04-30 10:19:59 +0800

    log

--- src/main/java/com/altersoftware/hotel/HotelApplication.java
+++ src/main/java/com/altersoftware/hotel/HotelApplication.java
@@ -2,15 +2,17 @@ package com.altersoftware.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HotelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HotelApplication.class, args);
-
+        System.out.println("(♥◠‿◠)ﾉﾞ  启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+            "  ___ _   _  ___ ___ ___  ___ ___    \n" +
+            " / __| | | |/ __/ __/ _ \\/ __/ __|    \n" +
+            " \\__ \\ |_| | (_| (_|  __/\\__ \\__ \\  \n" +
+            " |___/\\__,_|\\___\\___\\___||___/___/ \n");
     }
 
 
